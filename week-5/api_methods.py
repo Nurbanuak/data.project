ca3c88c36792840c44399819e66571c7cf35d1dc
Nurbanuak <banu.akca@hotmail.com>
2023-12-31 10:57:03 +0300

    updated commit

---
+++ week-5/api_methods.py
@@ -0,0 +1,56 @@
+import pandas as pd
+
+def get_channel_stats(youtube, channel_ids):
+    all_data = []
+
+    request = youtube.channels().list(
+        part="snippet,contentDetails,statistics",
+        id=",".join(channel_ids)
+    )
+
+    response = request.execute()
+
+    # loop through items in response
+    for item in response["items"]:
+        data = {'channelName': item['snippet']['title'],
+                'subscribers': item['statistics']['subscriberCount'],
+                'views': item['statistics']['viewCount'],
+                'totalVideos': item['statistics']['videoCount'],
+                'playlistId': item['contentDetails']['relatedPlaylists']['uploads']
+        }
+        all_data.append(data)
+    
+    return pd.DataFrame(all_data)
+
+
+def get_video_ids(youtube, playlist_id):
+    video_ids = []
+
+    request = youtube.playlistItems().list(
+            part="snippet,contentDetails",
+            playlistId=playlist_id,
+            maxResults=50
+        )
+    response = request.execute()
+
+    for item in response['items']:
+        video_ids.append(item['contentDetails']['videoId'])
+
+    next_page_token = response.get('nextPageToken')
+    
+    while next_page_token is not None:
+        request = youtube.playlistItems().list(
+                part="snippet,contentDetails",
+                playlistId=playlist_id,
+                maxResults=50,
+                pageToken=next_page_token
+            )
+        response = request.execute()
+
+        for item in response['items']:
+            video_ids.append(item['contentDetails']['videoId'])
+
+        next_page_token = response.get('nextPageToken')    
+
+        
+    return video_ids
